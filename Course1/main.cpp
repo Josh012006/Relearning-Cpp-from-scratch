3b20e29a963bd4b58b083c401a77f2a4c995eed7
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-10-12 03:08:10 -0400

    feat: Covering the basics

---
+++ Course1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+int main() {
+
+    int myAge = 19;
+    double PI = 3.14159;
+    bool isChill = true;
+
+    char myChar = 'J';
+    std::string myName = "Josh";
+
+    std::cout << "Hello! My name is " << myName << ". I'm " << myAge << " years old." << std::endl;
+    return 0;
+}
---
+++ Course1/sunnywarm.cpp
@@ -0,0 +1,15 @@
+/*
+ * Relearning C++ completely from the start
+ * Learner : Josué Mongan
+ */
+#include <iostream>
+
+int main() {
+    bool isSunny = false;
+    bool isWarm = false;
+
+    std::cout << std::boolalpha;
+
+    std::cout << "It's sunny : " << isSunny << std::endl;
+    std::cout << "It's warm : " << isWarm << std::endl;
+}
