f839d1a630b8b1f487c08a7f1a5224974f99f0a0
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-10-12 03:24:21 -0400

    feat: Logical operators

---
+++ Course1/logical.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+
+int main() {
+
+    std::cout << (true && false) << std::endl;
+    std::cout << (false || true) << std::endl;
+    std::cout << (!false) << std::endl;
+
+    return 0;
+}
