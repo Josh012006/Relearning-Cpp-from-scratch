7350164e671f365e8c353e67d98a703f7646b2ba
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-10-31 18:44:35 -0400

    feat: Refining it

--- Course3/tempconv.cpp
+++ Course3/tempconv.cpp
@@ -9,13 +9,13 @@ int main() {
     std::array<double, 7> temps;
 
     for(int i = 0; i < 7; i++) {
-        std::cout << "Enter a temperature in fahrenheit: ";
+        std::cout << "Enter a temperature in fahrenheit for day " << (i+1) << ": ";
         std::cin >> temps[i];
     }
 
     for(double temp : temps) {
         double celsius = (temp - 32) * 5 /9;
-        std::cout << temp << "°F    " << celsius << "°C" << std::endl;
+        std::cout << temp << " F    " << celsius << " C" << std::endl;
     }
 
     return 0;
