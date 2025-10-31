cc2f351afa509b7b70cfc5684601e301b8374655
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-10-31 18:38:03 -0400

    feat: project 4.1

---
+++ Course3/tempconv.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+
+
+
+
+int main() {
+
+    std::array<double, 7> temps;
+
+    for(int i = 0; i < 7; i++) {
+        std::cout << "Enter a temperature in fahrenheit: ";
+        std::cin >> temps[i];
+    }
+
+    for(double temp : temps) {
+        double celsius = (temp - 32) * 5 /9;
+        std::cout << temp << "°F    " << celsius << "°C" << std::endl;
+    }
+
+    return 0;
+}
